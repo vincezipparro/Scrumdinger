18d12c041eb422cd97d2fb23c4ef2c9f538cde0b
Vince Zipparro <vince.zipparro@legacymusicpartners.com>
2021-10-20 14:48:53 -0500

    help

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vince Zipparro on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
