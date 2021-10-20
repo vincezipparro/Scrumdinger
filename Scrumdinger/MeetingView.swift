b2a2ec33ef0fed58b55b61dd4be006f72b674690
Vince Zipparro <vince.zipparro@legacymusicpartners.com>
2021-10-20 15:12:20 -0500

    add progress bar and background color

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            ProgressView(value: 5, total: 15)
+        }
     }
+    
+    
 }
 
 struct MeetingView_Previews: PreviewProvider {
