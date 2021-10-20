680cca1deb36ba0e0d5fb0d641d973f373a5b2a7
Vince Zipparro <vince.zipparro@legacymusicpartners.com>
2021-10-20 15:21:44 -0500

    text and labels added

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ZStack{
-            Color.black
-                .ignoresSafeArea()
+        
+        VStack{
             ProgressView(value: 5, total: 15)
+            HStack{
+                VStack{
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack{
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
         }
+        
     }
     
     
