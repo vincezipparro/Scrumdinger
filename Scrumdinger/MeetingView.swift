f7a925c35934bbb3810a484daef813646e55749d
Vince Zipparro <vince.zipparro@legacymusicpartners.com>
2021-10-20 15:44:22 -0500

    add more compononents

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -6,18 +6,32 @@ struct MeetingView: View {
             ProgressView(value: 5, total: 15)
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
+                    Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 
                 Spacer()
                 
                 VStack(alignment: .trailing) {
-                    Text("Seconds Remaining").font(.caption)
+                    Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
