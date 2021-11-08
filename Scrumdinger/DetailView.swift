5909c294ab86544f544b35de12ad8b37bf1ed45b
Vince Zipparro <vince.zipparro@legacymusicpartners.com>
2021-11-08 16:30:39 -0600

    present edit model and add nav buttons

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrumModel
+    @State private var isPresented = false
     
     var body: some View {
         List{
@@ -45,7 +46,22 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetListStyle())
+        .navigationBarItems(trailing: Button("Edit"){
+            isPresented = true
+        })
         .navigationTitle("\(scrum.title)")
+        .fullScreenCover(isPresented: $isPresented){
+            NavigationView{
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel"){
+                            isPresented = false
+                    }, trailing: Button("Done"){
+                        isPresented = false
+                    }
+                )
+            }
+        }
     }
 }
 
